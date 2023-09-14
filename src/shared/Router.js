fc9c7508df37944b370098288d2abacf5d26ee17
최유리 <choeyuli@gimgyeoglogsMBP.lan>
2023-09-15 00:48:00 +0900

    favicon 변경

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Main from '../page/Main'
 import Layout from '../components/Layout'
+import GlobalStyles from '../style/GlobalStyle'
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <GlobalStyles />
       <Routes>
         <Route element={<Layout />}>
           <Route path={''} element={<Main />} />
